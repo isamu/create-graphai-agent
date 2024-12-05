d701b2ad0a70e7e68611028ba862c2032266d00d
isamu <isamu@to-kyo.to>
2024-12-05 17:57:00 +0900

    add template

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,50 @@
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 
-const main = () => {
+import prompts from 'prompts';
+
+const copyFile = (root: string, file: string) => {
+  const templateDir = path.resolve(__dirname, "..", "template")
+  fs.copyFileSync(path.resolve(templateDir, file), path.resolve(root, file))
+  
+}
+const mkdir = (path: string) => {
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path)
+  }
+};
+const main = async () => {
+  const defaultProjectName = "graphai-agent"
+  const result = await prompts(
+    [
+      {
+        name: 'agentName',
+        type: 'text',
+        message: 'agent name',
+        initial: defaultProjectName,
+        // onState: (state) => (targetDir = String(state.value).trim() || defaultProjectName),
+      },
+    ]
+  )
+  const agentName = result["agentName"].trim();
+  
   const cwd = process.cwd()
-  const targetDir = "graphai-agent";
+  const targetDir = agentName;
   const root = path.join(cwd, targetDir)
 
-  if (!fs.existsSync(root)) {
-    fs.mkdirSync(root)
-  }
+  mkdir(root);
+  mkdir(path.resolve(root, "src"))
+  mkdir(path.resolve(root, "tests"))
+
+  copyFile(root, "eslint.config.mjs");
+  copyFile(root, "tests/test_agent_runner.ts");
+  
   fs.writeFileSync(
     path.resolve(root, 'README.md'),
-    "# GraphAI Agent"
+    [
+      "# GraphAI Agent",
+      agentName,
+    ].join("\n")
   )
 };
 
