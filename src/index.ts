8ab6d97199686823d0cde924fe6c98595b41282a
isamu <isamu@to-kyo.to>
2024-12-05 17:06:26 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("HELLO");
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+
+const main = () => {
+  const cwd = process.cwd()
+  const targetDir = "123";
+  const root = path.join(cwd, targetDir)
+
+  if (!fs.existsSync(root)) {
+    fs.mkdirSync(root)
+  }
+  fs.writeFileSync(
+    path.resolve(root, 'README.md'),
+    "# GraphAI Agent"
+  )
+};
+
+main();
